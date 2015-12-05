f3eae98520be7c9a20ca16b82bc0fa807f5e7742
John Freeman <jfreeman08@gmail.com>
2015-12-05 13:02:31 -0500

    Solve day 3

---
+++ 03-houses/Main.hs
@@ -0,0 +1,30 @@
+import qualified Data.Set as S
+
+type Position = (Int, Int)
+type Direction = Char
+
+move1 :: Position -> Direction -> Position
+move1 (x,y) d = case d of
+  '<' -> (x-1,y)
+  '>' -> (x+1,y)
+  'v' -> (x,y-1)
+  '^' -> (x,y+1)
+  _   -> (x,y)
+
+locsVisited :: Position -> [Direction] -> S.Set Position
+locsVisited p ds = S.fromList $ scanl move1 p ds
+
+numVisited :: Position -> [Direction] -> Int
+numVisited p ds = S.size $ locsVisited p ds
+
+roundRobin :: Int -> [a] -> [[a]]
+roundRobin n xs =
+  let (hs, ts) = splitAt n xs
+  in zipWith (:) hs (roundRobin n ts ++ repeat [])
+
+numVisitedWithRobo :: Position -> [Direction] -> Int
+numVisitedWithRobo p = S.size . S.unions . map (locsVisited p) . roundRobin 2
+
+main :: IO ()
+-- main = getContents >>= print . numVisited (0,0)
+main = getContents >>= print . numVisitedWithRobo (0,0)
