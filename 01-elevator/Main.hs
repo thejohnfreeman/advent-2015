32062878154919715e415b0992332f5abfb655ed
John Freeman <jfreeman08@gmail.com>
2015-12-26 14:40:52 -0600

    Improve day 1

--- 01-elevator/Main.hs
+++ 01-elevator/Main.hs
@@ -1,16 +1,15 @@
 -- stack runghc --verbosity error
 
-import Control.Monad
-import Data.List
+import Data.Maybe (fromJust)
+import Data.List (findIndex, scanl1)
 
 delta :: Char -> Int
 delta '(' = 1
 delta ')' = -1
 delta _ = 0
 
-firstBasement :: String -> Maybe Int
-firstBasement = findIndex (<0) . scanl (+) 0 . map delta
-
 main :: IO ()
--- main = getContents >>= print . sum . map delta
-main = getContents >>= putStrLn . maybe "nothing" show . firstBasement
+main = do
+  ds <- map delta <$> getContents
+  print $ sum ds
+  print $ fromJust $ findIndex (<0) $ scanl1 (+) ds
