a4384e6789b3bdf706ed548ad70bc226d117cd4b
John Freeman <jfreeman08@gmail.com>
2015-12-28 16:16:57 -0600

    Improve day 3

--- 03-houses/Main.hs
+++ 03-houses/Main.hs
@@ -1,12 +1,15 @@
 -- stack runghc --verbosity error
 
+{-# LANGUAGE LambdaCase #-}
+
+import qualified Data.List as L
 import qualified Data.Set as S
 
 type Position = (Int, Int)
 type Direction = Char
 
 move1 :: Position -> Direction -> Position
-move1 (x,y) d = case d of
+move1 (x,y) = \case
   '<' -> (x-1,y)
   '>' -> (x+1,y)
   'v' -> (x,y-1)
@@ -19,10 +22,13 @@ locsVisited p ds = S.fromList $ scanl move1 p ds
 numVisited :: Position -> [Direction] -> Int
 numVisited p ds = S.size $ locsVisited p ds
 
+-- Which is better?
 roundRobin :: Int -> [a] -> [[a]]
-roundRobin n xs =
-  let (hs, ts) = splitAt n xs
-  in zipWith (:) hs (roundRobin n ts ++ repeat [])
+roundRobin n xs = L.transpose $ L.unfoldr f xs
+  where f xs = if null xs then Nothing else Just $ splitAt n xs
+-- roundRobin n xs =
+--   let (hs, ts) = splitAt n xs
+--   in zipWith (:) hs (roundRobin n ts ++ repeat [])
 
 numVisitedWithRobo :: Position -> [Direction] -> Int
 numVisitedWithRobo p = S.size . S.unions . map (locsVisited p) . roundRobin 2
