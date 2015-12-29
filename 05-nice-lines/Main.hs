cca0370385a014e7febb4b7227947d31f6dbff6c
John Freeman <jfreeman08@gmail.com>
2015-12-29 00:33:56 -0600

    Use regexes

--- 05-nice-lines/Main.hs
+++ 05-nice-lines/Main.hs
@@ -1,31 +1,27 @@
--- stack runghc --verbosity error
+-- stack runghc --verbosity error --package text --package text-icu
+
+{-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad (forM_)
-import Data.List (isInfixOf, group)
+import qualified Data.List as L
+import Data.Maybe (isJust, isNothing)
+import qualified Data.Text as T
+import qualified Data.Text.ICU as U
+import qualified Data.Text.IO as TIO
 
 preds1 =
-  [ (>=3) . length . filter (`elem` "aeiou")
-  , (>=2) . maximum . map length . group
-  , not . or . zipWith ($) (map isInfixOf ["ab", "cd", "pq", "xy"]) . repeat
+  [ (>=3) . length . U.findAll (U.regex [] "a|e|i|o|u")
+  , isJust . U.find (U.regex [] "(.)\\1")
+  , isNothing . U.find (U.regex [] "ab|cd|pq|xy")
   ]
 
-hasPairs :: String -> Bool
-hasPairs (x:y:xs) = [x,y] `isInfixOf` xs || hasPairs (y:xs)
-hasPairs _ = False
-
-hasSplitRepeat :: String -> Bool
-hasSplitRepeat (x:y:z:xs) = x == z || hasSplitRepeat (y:z:xs)
-hasSplitRepeat _ = False
-
-preds2 = [hasPairs, hasSplitRepeat]
+preds2 = [ isJust . U.find (U.regex [] p) | p <- ["(..).*\\1", "(.).\\1"] ]
 
 matchesAll :: [a -> Bool] -> a -> Bool
 matchesAll preds = and . zipWith ($) preds . repeat
 
 main :: IO ()
 main = do
-  ls <- lines <$> getContents
+  ls <- T.lines <$> TIO.getContents
   forM_ [preds1, preds2] $ \preds ->
     print $ length $ filter (matchesAll preds) ls
-  -- print $ hasPairs "qjhvhtzxzqqjkmpb"
-
